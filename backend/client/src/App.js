b01adb34fc6b2a5e4d02be6b6beebdd6fdf922f5
Junaid <161832615+Ken-Kanike@users.noreply.github.com>
2025-01-22 19:59:07 +0530

    Initial Template Done

---
+++ backend/client/src/App.js
@@ -0,0 +1,14 @@
+
+import Home from "./components/Home";
+function App() {
+  return (
+    <>
+      {/* <Header />*/}
+      {/* <Main /> */}
+      <Home />
+      {/* <Footer />  */}
+    </>
+  );
+}
+
+export default App;
